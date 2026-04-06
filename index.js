2f825736650223af16d95df9abbc2f8ca11608f3
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-04-06 19:19:24 +0100

    Exersice 4.1

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 alert("Hello World!")
 
-let f2= (a,b,n) =>{
-    let total=0;
-    for(let i=0; i<n; i++){
-        if (i%a==0||i%b==0){
-            total+=i
+let f2 = (a, b, n) => {
+    let total = 0;
+    for (let i = 0; i < n; i++) {
+        if (i % a == 0 || i % b == 0) {
+            total += i
         }
     }
     return total;
 }
 
-alert(f2(3,5,1000))
+alert(f2(3, 5, 1000))
 
-let f3handler=()=>{
-    let a=document.getElementById('a').value;
-    let b=document.getElementById('b').value;
-    let n=document.getElementById('n').value;
-    alert(f2(a,b,n));
+let f3handler = () => {
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    let n = document.getElementById('n').value;
+    alert(f2(a, b, n));
+}
+
+function f4(a, b, l) {
+    let sum = 0;
+
+    for (let i = 0; i < l.length; i++) {
+        if (l[i] % a === 0 || l[i] % b === 0) {
+            sum += l[i];
+        }
+    }
+
+    return sum;
+}
+
+let arr = [1, 3, 5, 7, 9, 10];
+let results = f4(3, 5, arr);
+
+alert(results);
+
+let f4handler = () => {
+    let a = parseInt(document.getElementById("a").value);
+    let b = parseInt(document.getElementById("b").value);
+
+    let l = document.getElementById("l").value;
+
+    // convert input text into array
+    let arr = l.split(" ");
+
+    let result = f4(a, b, arr);
+
+    alert(result);
 }
